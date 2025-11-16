5dd99d4a7c620a7d7c0bb0595e6bb0f6f0d60b68
Nicolaspresiga <presiga512@gmail.com>
2025-11-16 10:57:46 -0500

    Delete punto4.py

--- punto4.py
+++
@@ -1,8 +0,0 @@
-
-def pares(n, m):
-    if n <= 0 or m <= 0 or n >= m:
-        raise Exception("No es posible continuar con la operación")
-
-    for numero in range(n, m + 1):
-        if numero % 2 == 0:
-            yield numero
